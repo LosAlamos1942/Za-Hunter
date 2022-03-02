436ac3918b8e7c14b92fc40c2d8d482d9b002c48
Nerto Kageh <kagehnerto@yahoo.com>
2022-03-02 02:36:34 -0600

    Added map search feature.

--- Za Hunter/ContentView.swift
+++ Za Hunter/ContentView.swift
@@ -10,28 +10,54 @@ import MapKit
 
 struct ContentView: View {
     @State private var region = MKCoordinateRegion(
-            center: CLLocationCoordinate2D(
-                latitude: 42.15704,
-                longitude: -88.14812),
-            span: MKCoordinateSpan(
-                latitudeDelta: 0.05,
-                longitudeDelta: 0.05)
-        )
+        center: CLLocationCoordinate2D(
+            latitude: 42.15704,
+            longitude: -88.14812),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05)
+    )
     @StateObject var locationManager = LocationManager()
-        @State private var userTrackingMode: MapUserTrackingMode = .follow
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
+    @State private var places = [Place]()
     
     var body: some View {
         Map(
-                    coordinateRegion: $region,
-                    interactionModes: .all,
-                    showsUserLocation: true,
-                    userTrackingMode: $userTrackingMode
-                )
+            coordinateRegion: $region,
+            interactionModes: .all,
+            showsUserLocation: true,
+            userTrackingMode: $userTrackingMode,
+            annotationItems: places) { place in
+            MapPin(coordinate: place.annotation.coordinate)
+        }
+        .onAppear(perform: {
+            performSearch(item: "Pizza")
+        })
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    func performSearch(item: String) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = item
+        searchRequest.region = region
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { response, error in
+            if let response = response {for mapItem in response.mapItems {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = mapItem.placemark.coordinate
+                annotation.title = mapItem.name
+                places.append(Place(annotation: annotation, mapItem: mapItem))
+                }
+            }
+        }
+    }
+    struct Place: Identifiable {
+        let id = UUID()
+        let annotation: MKPointAnnotation
+        let mapItem: MKMapItem
     }
 }
